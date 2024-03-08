98a9c454540abf8bbca918a0639535ab38e47892
jessiarazi <jessiarazi22@gmail.com>
2024-03-08 12:21:45 -0300

    Agrego avances de juego

--- tp-final/script.js
+++ tp-final/script.js
@@ -34,6 +34,24 @@ function validarFormulario(){
     
 }
 
-function iniciar(){
-    
-}
+//FUNCIONES JUEGO
+    var preguntas = ['¿Quién fue la primera mujer en encadenar un 9a+?','¿Qué vía de escalada es la más difícil del mundo (9c)?','¿Cuál de los siguientes no es un tipo de agarre común en la escalada?','¿Qué es un "boulder" en el contexto de la escalada?',' ¿Qué significa "Gri-Gri" en el mundo de la escalada?'];
+    var puntaje = 10; //el juego inicia con un puntaje mayor a 0 así si no contesta bien la primera pregunta, no se reinicia automáticamente
+    var indice = 0;
+    var contenido;
+    var r = 0;
+
+    function iniciar(){
+        document.getElementById('boton-inicio').style.display = 'none'; //reemplaza el contenido del botón comenzar por "siguiente pregunta"
+        sortearpregunta(); //genera una nueva pregunta a partir de la función sortearpregunta()
+        document.getElementById('titulo').innerHTML = pregunta; //reemplaza el contenido del título por una pregunta
+        document.getElementById('points').style.display = 'block';
+        document.getElementById('pregunta').style.display = 'none'; 
+        document.getElementById('puntaje').innerHTML = 'Tu puntaje es: '+ puntaje +' puntos'; //muestra el puntaje en el id "puntaje"
+
+    }
+
+    function sortearpregunta(){
+        pregunta = preguntas[indice]; //toma las preguntas del array "preguntas"
+
+    }
